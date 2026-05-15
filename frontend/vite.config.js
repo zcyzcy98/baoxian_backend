b73240d6a13b9992e5b082c5b2c5fdfa1adda6c7
zhangchengyuan <1006850364@qq.com>
2026-05-15 11:41:43 +0800

    fix: 更新vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    cssMinify: false,
+  },
   server: {
     port: 5173,
     host: true,
